b39c3654db4fef8130812c85f511a8c702964d77
James11T <jamail090999@gmail.com>
2022-10-31 22:55:42 +0000

    addded country code hint

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,11 +1,11 @@
 import * as FlagCommand from "./flag";
 import * as SimilarityCommand from "./similarity";
 import * as SkipCommand from "./skip";
+import * as CodeCommand from "./code";
 import { Collection } from "discord.js";
-
 import type { Command } from "../types";
 
-const commands = [FlagCommand, SimilarityCommand, SkipCommand];
+const commands = [FlagCommand, SimilarityCommand, SkipCommand, CodeCommand];
 const commandCollection = new Collection<string, Command>();
 commands.forEach((cmd) => commandCollection.set(cmd.command.name, cmd));
 
